7367b7891d798ebe7b1d9359c0732cfd9d427804
hpeter19 <njoshpitar@gmail.com>
2025-03-18 17:51:35 +0300

    barcharts

---
+++ barcharts.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = ["c++","Java","Go","python"]
+y =[20,38,29,59]
+#line graph
+
+plt.bar(x,y, color="g",align="edge", width=0.6,edgecolor="b",)
+plt.show()
+
+
