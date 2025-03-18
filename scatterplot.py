634cfb5cbb53cdeb100198ce6410908206a3d6d2
hpeter19 <njoshpitar@gmail.com>
2025-03-18 09:50:14 +0300

    scatter plot using random variables

---
+++ scatterplot.py
@@ -0,0 +1,10 @@
+#generating a scatter plot with random data using matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+x_data= np.random.random(50)*100
+y_data = np.random.random(50) * 100
+plt.scatter(x_data,y_data)
+plt.show()
