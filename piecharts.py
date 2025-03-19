d03db825321fe135efece190b673ba68bf5d62dc
hpeter19 <njoshpitar@gmail.com>
2025-03-19 12:29:20 +0300

    pulling components from a chart a little bit

--- piecharts.py
+++ piecharts.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 
 lang =["Python","Java","C##","C","Javascript"]
 points=[40,30,15,9,6]
+#pulling items from the chart to highlight them a little bit
+explodes=[0.3,0,0,0,0]
 
-plt.pie(points,labels=lang)
+plt.pie(points,labels=lang ,explode=explodes)
 plt.show()
