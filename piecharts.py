26fcf341d74e52f1ca29fe952ad0756690ce4e3a
hpeter19 <njoshpitar@gmail.com>
2025-03-19 12:43:15 +0300

    percentages, percentage distance and starting angle

--- piecharts.py
+++ piecharts.py
@@ -5,6 +5,12 @@ lang =["Python","Java","C##","C","Javascript"]
 points=[40,30,15,9,6]
 #pulling items from the chart to highlight them a little bit.Cracking out a little bit
 explodes=[0,0,0,0,0.2]
+#writing percentages outside the charts
+#setting percentage distance outside the charts
+#starting angle
 
-plt.pie(points,labels=lang ,explode=explodes)
+plt.pie(points,labels=lang ,explode=explodes,
+        autopct="%.2f%%S",
+        pctdistance=1.62,
+        startangle=90)
 plt.show()
