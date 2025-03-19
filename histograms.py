6ff57ff70273da964b5f568b0ca12f30363ef548
hpeter19 <njoshpitar@gmail.com>
2025-03-19 12:17:04 +0300

    cumulative histograms

--- histograms.py
+++ histograms.py
@@ -1,9 +1,8 @@
-#using random values generate by numpy
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 ages=np.random.normal(20,1.5,100)
-plt.hist(ages)
+plt.hist(ages, bins=10, cumulative=True)
+#comulative histograms
 plt.show()
 
