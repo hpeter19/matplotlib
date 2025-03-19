4ce5c5532d1d892b145ff41cf66456b021f05a01
hpeter19 <njoshpitar@gmail.com>
2025-03-19 12:31:15 +0300

    cracking up a little bit a component

--- piecharts.py
+++ piecharts.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 lang =["Python","Java","C##","C","Javascript"]
 points=[40,30,15,9,6]
-#pulling items from the chart to highlight them a little bit
-explodes=[0.3,0,0,0,0]
+#pulling items from the chart to highlight them a little bit.Cracking out a little bit
+explodes=[0,0,0,0,0.2]
 
 plt.pie(points,labels=lang ,explode=explodes)
 plt.show()
