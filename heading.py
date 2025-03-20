6e9b138b72722aefcbe930efce0466e8c8c2293c
hpeter19 <njoshpitar@gmail.com>
2025-03-20 15:57:04 +0300

    Formatting the text

--- heading.py
+++ heading.py
@@ -5,7 +5,8 @@ years= [2016,2017,2018,2019,2020,2021,2022,2023]
 income =[56,61,65,70,72,76,82,90]
 income_ticks =list(range(50,81,2))
 plt.plot(years,income)
-plt.title("Income of John(In Ksh)")
+#editing the title font size and font family
+plt.title("Income of John(In Ksh)", fontsize=22, fontfamily="FreeSerif")
 plt.xlabel("Year")
 plt.ylabel("Income in Ksh")
 plt.yticks(income_ticks,[f"{x}k Ksh" for x in income_ticks])
