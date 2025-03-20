9b511838dff015c7cd2159d8f436a3a829a46393
peter njjosh <njoshpitar@gmail.com>
2025-03-20 14:44:13 +0300

    Update barcharts.py

--- barcharts.py
+++ barcharts.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 x = ["c++","Java","Go","python"]
 y =[20,38,29,59]
-#line graph
+#Barchart
 
 plt.bar(x,y, color="g",align="edge", width=0.6,edgecolor="b",)
 plt.show()
